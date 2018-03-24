598c5e730e87242a30dbf6a09ecb2a3ac954fa9b
Karan171996 <krnsngh38@gmail.com>
2018-03-24 22:56:42 +0530

    Making Github Repos for output processing

---
+++ src/main/java/com/disaster/management/OutputProcessingApplication.java
@@ -0,0 +1,12 @@
+package com.disaster.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutputProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutputProcessingApplication.class, args);
+	}
+}
